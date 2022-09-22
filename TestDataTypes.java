69c8ce96e68676e29d0d51f9427b82599ad621cc
PRASHANT SRIVASTAV <113378187+Praasu07@users.noreply.github.com>
2022-09-22 18:38:35 +0530

    Add files via upload

---
+++ TestDataTypes.java
@@ -0,0 +1,29 @@
+package datatypes;
+
+public class TestDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a= 127;
+		short b= 2323;
+		int c= 45645567;
+		long d= 4564556767l;
+		float e= 12.5f;
+		double f= 323.5;
+		boolean g= true;
+		char h= 'A';
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		
+		
+
+	}
+
+}
