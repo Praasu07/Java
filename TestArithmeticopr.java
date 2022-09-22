289aa9e22ab669f7682dad6a7c67b5899d48c83b
PRASHANT SRIVASTAV <113378187+Praasu07@users.noreply.github.com>
2022-09-22 18:35:52 +0530

    Add files via upload

---
+++ TestArithmeticopr.java
@@ -0,0 +1,33 @@
+package opr;
+
+public class TestArithmeticopr {
+
+public static void main(String args[]) {
+	int i=27,j=22;
+	 System.out.println("Variable values...");
+	 System.out.println("i =" + i);
+	 System.out.println("j =" + j);
+	 
+	 //adding numbers
+	 System.out.println("Addition...");
+	 System.out.println(" i + j =" + (i + j));
+	 
+	 //subtracting numbers
+	 System.out.println("Subtracting...");
+	 System.out.println(" i - j =" + (i - j));
+	 
+	 //multiplying numbers
+	 System.out.println("Multiplying...");
+	 System.out.println(" i * j =" + (i * j));
+	 
+	 //dividing numbers
+	 System.out.println("Dividing...");
+	 System.out.println(" i / j =" + (i / j));
+	 
+	 //computing the remainder
+	 System.out.println("Remainder...");
+	 System.out.println(" i % j =" + (i % j));
+	 
+}
+
+}
